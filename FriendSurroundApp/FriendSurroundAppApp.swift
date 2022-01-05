6beee8f04a185f73856919a43d07833124e4d385
anita908 <anita248846@gmail.com>
2022-01-05 16:56:04 -0700

    Initial Commit

---
+++ FriendSurroundApp/FriendSurroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendSurroundAppApp.swift
+//  FriendSurroundApp
+//
+//  Created by 吳若瑀 on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendSurroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
