1e7b6a10053708b48d1bf539ce967b435e8110c5
anita908 <anita248846@gmail.com>
2022-02-08 18:11:56 -0700

    commit

--- FriendSurroundApp/View/App/FriendSurroundAppApp.swift
+++ FriendSurroundApp/View/App/FriendSurroundAppApp.swift
@@ -12,8 +12,8 @@ struct FriendSurroundAppApp: App {
     var body: some Scene {
         WindowGroup {
 //            MenuView().environmentObject(FriendViewModel())
-//            SetUpPhoneNumberView()
-            notification()
+            SetUpPhoneNumberView()
+//            notification()
         }
     }
 }
